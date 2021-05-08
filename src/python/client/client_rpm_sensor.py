add681ed2c52f0b0f73baa77001366e81693f421
NelsonIg <nelson.igbokwe@web.de>
2021-05-08 18:02:32 +0200

    multithreading for polling rpm

--- src/python/client/client_rpm_sensor.py
+++ src/python/client/client_rpm_sensor.py
@@ -59,7 +59,7 @@ def clalc_time_diff():
         else:
             counter +=1
             if counter>4: diff_vec = np.zeros(n_pulses)
-        mean_diff.value = diff_vec.mean()
+        mean_diff.value = int(diff_vec.mean())
         print(f'Thread\tdiff: {diff}')
         print(f'Thread\tdiff_vec: {diff_vec}')
         print(f'Thread\tmean_diff: {mean_diff.value}')
