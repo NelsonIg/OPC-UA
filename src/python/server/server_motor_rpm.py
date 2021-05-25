82a5c2c7ca0fb8555bcc4cd5d9768a27367e0588
NelsonIg <nelson.igbokwe@web.de>
2021-05-25 11:43:53 +0200

    removed '/server_exemple' from endpoint

--- src/python/server/server_motor_rpm.py
+++ src/python/server/server_motor_rpm.py
@@ -153,7 +153,7 @@ async def main(host='localhost'):
     # init server, set endpoint
     server = Server()
     await server.init()
-    server.set_endpoint(f"opc.tcp://{host}:4840/server_example/")
+    server.set_endpoint(f"opc.tcp://{host}:4840")
 
     # setup of namespace, not needed
     uri = "example-uri.edu"
