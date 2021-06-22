82579315d330073b7498763e95a3b8ea5319ce08
NelsonIg <nelson.igbokwe@web.de>
2021-06-22 18:36:48 +0200

    update

--- src/matlab/read_write_opcua.m
+++ src/matlab/read_write_opcua.m
@@ -1,29 +1,23 @@
 function [outputVal] = read_write_opcua(inputVal)
 % - Connect to opcua server, if not connected
+% - Write inputVal to opcua variable% 
 % - Read from opcua variable and return value
-% - Write inputVal to opcua variable
 
-% endpoint: opc.tcp://localhost:4840/server_example/
 
 persistent uaClient; % opcua client
 persistent initClient; % variable to track initialised clients
-persistent rpm_node;
-persistent inp_node;
-persistent initNodes;
+persistent rpmNode; % OPC UA Node for rpm of motor
+persistent inpNode; % OPC UA Node for motor input [0-1]
+persistent initNodes; % variable to track initialised Nodes
 
 host = '192.168.0.183';
 port = 4840;
 
 % Connect to server if no client was initialised
 if (isempty(initClient))
-  
-    try
-        uaClient = opcua(host, port);
-        connect(uaClient);
-        initClient = 1; 
-    catch exception
-        warning('Connection to opcua client failed');
-    end
+    uaClient = opcua(host, port);
+    connect(uaClient);
+    initClient = 1; 
 % reconnect if connection lost
 elseif uaClient.isConnected == 0
     connect(uaClient);
@@ -35,17 +29,17 @@ end
 if isempty(uaClient) == 0
     % get nodes, if no nodes are already defined
     if uaClient.isConnected == 1 && (isempty(initNodes))
-        rpm_node = opcuanode(2, 5);
-        inp_node = opcuanode(2, 6);
+        rpmNode = opcuanode(2, 5);
+        inpNode = opcuanode(2, 6);
         initNodes = 1;
     end
 
 
     % read from & read to server
     if uaClient.isConnected == 1 && (isempty(initNodes)) == 0
-        writeValue(uaClient,inp_node, inputVal);
-        % pause(0.100);
-        [rpmVal, ~, ~] = readValue(uaClient, rpm_node);
+        writeValue(uaClient,inpNode, inputVal);
+        % pause(0.100); time for the motor to react --> not needed
+        [rpmVal, ~, ~] = readValue(uaClient, rpmNode);
 
     outputVal = double(rpmVal);
 
